d0c48209bbefcd467b10b9475dd717aed2cab4b2
Filipe Mangue <43821186+FilipeMangue@users.noreply.github.com>
2026-05-23 16:13:58 +0200

    Create Helpers.php

---
+++ app/Core/Helpers.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Core\Config;
+
+function e(?string $value): string
+{
+    return htmlspecialchars((string) $value, ENT_QUOTES, 'UTF-8');
+}
+
+function url(string $path = ''): string
+{
+    $base = rtrim(Config::get('app_url'), '/');
+    return $base . '/' . ltrim($path, '/');
+}
+
+function asset(string $path): string
+{
+    return url('assets/' . ltrim($path, '/'));
+}
+
+function redirect(string $path): never
+{
+    header('Location: ' . url($path));
+    exit;
+}
